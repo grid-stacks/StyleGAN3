94706f7a3ad5056043662944933f0dbf132b37b5
DHN Chandan <php.chandan@gmail.com>
2022-04-13 13:22:01 +0600

    refactor: Add gpu list endpoint

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import nvsmi
+
 from fastapi import FastAPI
 
 app = FastAPI(title="StyleGAN3 API")
@@ -6,3 +8,9 @@ app = FastAPI(title="StyleGAN3 API")
 @app.get("/")
 def home():
     return {"hello": "world"}
+
+
+@app.get("/gpu_type")
+def gpu_type():
+    return {"gpus": list(nvsmi.get_gpus()), "available_gpus": list(nvsmi.get_available_gpus()),
+            "gpu_processes": nvsmi.get_gpu_processes()}
