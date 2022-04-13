24d9146c264239a59ff662fc87e0bf0cfa649433
DHN Chandan <php.chandan@gmail.com>
2022-04-14 02:24:29 +0600

    refactor: Add torch device endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 import nvsmi
+import torch
+import sys
 
 from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI(title="StyleGAN3 API")
 
@@ -14,3 +17,10 @@ def home():
 def gpu_type():
     return {"gpus": list(nvsmi.get_gpus()), "available_gpus": list(nvsmi.get_available_gpus()),
             "gpu_processes": nvsmi.get_gpu_processes()}
+
+
+@app.get("/torch_device")
+def torch_device():
+    device = torch.device('cuda:0')
+    print('Using device:', device, file=sys.stderr)
+    return {"device": str(device)}
